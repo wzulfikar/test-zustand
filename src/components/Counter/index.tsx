f50541e08220cc829e328952cc4f1195bbfcdbe8
Wildan Zulfikar <hey@wzulfikar.com>
2022-04-25 16:37:57 +0700

    Move store to external file

--- src/components/Counter/index.tsx
+++ src/components/Counter/index.tsx
@@ -1,19 +1,4 @@
-import create from "zustand";
-import { devtools } from "zustand/middleware";
-
-interface CounterStore {
-  count: number;
-  inc: () => void;
-  dec: () => void;
-}
-
-export const useStore = create<CounterStore>()(
-  devtools((set, get) => ({
-    count: 0,
-    inc: () => set({ count: get().count + 1 }),
-    dec: () => set({ count: get().count - 1 }),
-  }))
-);
+import { useStore } from "./store";
 
 export function Counter() {
   const { count, inc, dec } = useStore();
