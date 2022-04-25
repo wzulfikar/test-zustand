b11c41cc9ca8e9516f8e279decc79f1b8c6a48dd
Wildan Zulfikar <hey@wzulfikar.com>
2022-04-25 17:47:18 +0700

    Fix test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('render counter component', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const counterEl = screen.getByText(/count:/i);
+  expect(counterEl).toBeInTheDocument();
 });
